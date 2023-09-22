1d449be215090d1b9f11632570386583b3d498b3
ggjiuw <herezoom1231@gmail.com>
2023-09-22 13:22:28 +0300

    homework

--- api_google.py
+++ api_google.py
@@ -1,8 +1,5 @@
 import requests
 
-total_price = 0
-total_price_with_gluten = 0
-
 url = 'https://script.googleusercontent.com/macros/echo?user_content_key=6UqqJlqhXRB3t9I9UbzUryw2FXeLhvQS8_tjbbLQbMRGMoUvZZQ5xtDI_KbcgRwWv4Owoceo8Jd0zjvRLJP7kLiVyOypIFGam5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnOR8jsPZSYeAcj4w9rCMPYZpyQfjxt9FTrhvZE4B-g6ZhOMRYA1Pk_JohCJrhzlHnCusAfKPmohxr9Ljjvp9hyNAZYHaRiIttg&lib=MvOR8-gJfQC4LBGr_gYUOmQSW3VxzywJF'
 
 limit_of_products = requests.get(url)
@@ -17,9 +14,8 @@ response = requests.get(url=url, params=params)
 result = response.json()
 products = result['trip']
 
-for product in products:
-    total_price += product['price']
-    if product['contains_gluten'] == True:
-        total_price_with_gluten += product['price']
+total_price = sum([product['price'] for product in products])
+total_price_with_gluten = sum([product['price'] for product in products if product['contains_gluten'] == True])
 
-# print(f'загальна вартість усіх товарів: {total_price} грн\nа з глютеном: {total_price_with_gluten} грн')
+# print(total_price)
+# print(total_price_with_gluten)
